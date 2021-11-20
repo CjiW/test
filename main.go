dbf16b1b2b26301fcf62d58aa68e65ecc9e7799e
cjw <64598696+cao-jing-wei@users.noreply.github.com>
2021-11-20 18:09:22 +0800

    feat

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func main() {
+	r := gin.Default()
+	r.POST("/", func(context *gin.Context) {
+		context.JSON(200,gin.H{"msg":"ok","time":time.Now()})
+	})
+	_=r.Run(":8081")
+}
