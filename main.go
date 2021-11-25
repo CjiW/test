dfc183190c7c5942817424537421918898994d90
cjw <64598696+cao-jing-wei@users.noreply.github.com>
2021-11-25 15:50:30 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.POST("/", func(context *gin.Context) {
+	r.POST("/test", func(context *gin.Context) {
 		context.JSON(200,gin.H{"msg":"ok","time":time.Now()})
 	})
 	_=r.Run(":8089")
